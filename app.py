65f5886f800435aca4e655dbc2fa45769d6b4f77
KobDias <sofhia.dias@etec.sp.gov.br>
2025-03-24 13:53:52 -0300

    Completando o exemplo generico

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from funcao import prediz
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from waitress import serve
 
 app = Flask(__name__)
 
 # routes
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
 @app.route('/api/classify/iris', methods=['POST'])
 def iris_classifier_api():
     return jsonify({
         "species": prediz(request.json['sepal_length'], request.json['sepal_width'], request.json['petal_length'], request.json['petal_width'])
         })
+
+@app.route('/api/classify/iris', methods=['GET'])
+def iris_classifier_get():
+    return render_template('index.html')
+
+@app.route('/api/classify/iris', methods=['POST'])
+def iris_classifier():
+    return render_template('index.html', especie=species, )
+    
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
--- funcao.py
+++ funcao.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import joblib
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 def create_model():
     iris = pd.read_csv('iris.csv')
@@ -11,24 +13,25 @@ def create_model():
     y = iris[y_columns].values.ravel()
 
 
-    from sklearn.model_selection import train_test_split
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=101)
     X_train.shape
     X_test.shape
 
-    from sklearn.neighbors import KNeighborsClassifier
+
 
     knn = KNeighborsClassifier(n_neighbors=5)
     knn.fit(X_train, y_train)
     preds = knn.predict(X_test)
     preds
 
-    from sklearn.metrics import accuracy_score
+    
     joblib.dump(knn, 'knn.pkl')
+
 def prediz(sep_l, sep_w, pet_l, pet_w):
     if not os.path.isfile('knn.pkl'):
         create_model() # Certifica que o arquivo pkl existe
 
     modelo = joblib.load('knn.pkl') # Carregando o modelo
-    especie = modelo.predict([[sep_l, sep_w, pet_l, pet_w]]) # Predizendo a espécie
+    especie = modelo.prediz([[sep_l, sep_w, pet_l, pet_w]]) # Predizendo a espécie
     return especie[0]
